9b66889663f9be04d002ed665b5fad2ab4c7b15a
Christian <you@example.com>
2024-11-24 14:22:02 +0100

    twilio pakke

--- Routes/twilio.js
+++ Routes/twilio.js
@@ -3,7 +3,6 @@ require("dotenv").config();
 const app = require("express")();
 const bodyParser = require("body-parser");
 
-
 // Twilio API nøgler
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
