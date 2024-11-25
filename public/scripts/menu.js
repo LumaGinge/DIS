b2fd92c1cf0416444965452dd4ca98a7e3074168
Christian <you@example.com>
2024-11-25 17:32:24 +0100

    priser på produkter

--- public/scripts/menu.js
+++ public/scripts/menu.js
@@ -2,63 +2,78 @@
 const productList = [
     {
         productName: "Turkey",
-        imgsrc: "https://res.cloudinary.com/dlmwcu9am/image/upload/v1731251542/tcmfzr4uchvzfqkmhspc.png"
+        imgsrc: "https://res.cloudinary.com/dlmwcu9am/image/upload/v1731251542/tcmfzr4uchvzfqkmhspc.png",
+        price: 75.00
     },
     {
         productName: "JOE's Club",
-        imgsrc: "https://res.cloudinary.com/dlmwcu9am/image/upload/v1731251348/yfrhnotuvxtrxdqtp2uh.png"
+        imgsrc: "https://res.cloudinary.com/dlmwcu9am/image/upload/v1731251348/yfrhnotuvxtrxdqtp2uh.png",
+        price: 75.00
     },
     {
         productName: "Serrano",
-        imgsrc: "https://res.cloudinary.com/dlmwcu9am/image/upload/v1731251349/sv18ngadtqvspyx8thgg.png"
+        imgsrc: "https://res.cloudinary.com/dlmwcu9am/image/upload/v1731251349/sv18ngadtqvspyx8thgg.png",
+        price: 75.00
     },
     {
         productName: "Tunacado",
-        imgsrc: "https://res.cloudinary.com/dlmwcu9am/image/upload/v1731251349/f4tx0ebqyorf0sdfwgez.png"
+        imgsrc: "https://res.cloudinary.com/dlmwcu9am/image/upload/v1731251349/f4tx0ebqyorf0sdfwgez.png",
+        price: 75.00
     },
     {
         productName: "Tunacado",
-        imgsrc: "https://res.cloudinary.com/dlmwcu9am/image/upload/v1731251349/f4tx0ebqyorf0sdfwgez.png"
+        imgsrc: "https://res.cloudinary.com/dlmwcu9am/image/upload/v1731251349/f4tx0ebqyorf0sdfwgez.png",
+        price: 75.00
     },
     {
         productName: "Americano",
-        imgsrc: "https://res.cloudinary.com/dlmwcu9am/image/upload/v1731251799/ehwbv5me6c8x5ui9yjb5.png"
+        imgsrc: "https://res.cloudinary.com/dlmwcu9am/image/upload/v1731251799/ehwbv5me6c8x5ui9yjb5.png",
+        price: 36.00
     },
     {
         productName: "Latte",
-        imgsrc: "https://res.cloudinary.com/dlmwcu9am/image/upload/v1731251798/o36km7nmdasewogiqby7.png"
+        imgsrc: "https://res.cloudinary.com/dlmwcu9am/image/upload/v1731251798/o36km7nmdasewogiqby7.png",
+        price: 55.00
     },
     {
         productName: "Cappuccino",
-        imgsrc: "https://res.cloudinary.com/dlmwcu9am/image/upload/v1731251799/gcokdg58mqqdjebvibpv.png"
+        imgsrc: "https://res.cloudinary.com/dlmwcu9am/image/upload/v1731251799/gcokdg58mqqdjebvibpv.png",
+        price: 49.00
     },
     {
         productName: "Iced Latte",
-        imgsrc: "https://res.cloudinary.com/dlmwcu9am/image/upload/v1731251799/khpiz58iehs33qy3rmtf.png"
+        imgsrc: "https://res.cloudinary.com/dlmwcu9am/image/upload/v1731251799/khpiz58iehs33qy3rmtf.png",
+        price: 55.00
     },
     {
         productName: "Espresso",
-        imgsrc: "https://res.cloudinary.com/dlmwcu9am/image/upload/v1731251846/lza6wa3pz1pqxabus0ev.png"
+        imgsrc: "https://res.cloudinary.com/dlmwcu9am/image/upload/v1731251846/lza6wa3pz1pqxabus0ev.png",
+        price: 30.00
     }, 
     {
         productName: "Power Shake",
-        imgsrc: "https://res.cloudinary.com/dlmwcu9am/image/upload/v1731252529/ulussiugkzsoufowpwtu.png"
+        imgsrc: "https://res.cloudinary.com/dlmwcu9am/image/upload/v1731252529/ulussiugkzsoufowpwtu.png",
+        price: 65.00
     },
     {
         productName: "Pick Me up",
-        imgsrc: "https://res.cloudinary.com/dlmwcu9am/image/upload/v1731252529/tpip8swfipxcwwxhg5rm.png"
+        imgsrc: "https://res.cloudinary.com/dlmwcu9am/image/upload/v1731252529/tpip8swfipxcwwxhg5rm.png",
+        price: 65.00
     },
     {
         productName: "Green Tonic",
-        imgsrc: "https://res.cloudinary.com/dlmwcu9am/image/upload/v1731252530/a76cogivnaezmnoejqrb.png"
+        imgsrc: "https://res.cloudinary.com/dlmwcu9am/image/upload/v1731252530/a76cogivnaezmnoejqrb.png",
+        price: 65.00
     },
     {
-        productName: "Go Away DC",
-        imgsrc: "https://res.cloudinary.com/dlmwcu9am/image/upload/v1731252530/k56vdyrdgren5q2wgadl.png"
+        productName: "Go Away DOC",
+        imgsrc: "https://res.cloudinary.com/dlmwcu9am/image/upload/v1731252530/k56vdyrdgren5q2wgadl.png",
+        price: 65.00
     },
     {
         productName: "Sports Juice",
-        imgsrc: "https://res.cloudinary.com/dlmwcu9am/image/upload/v1731252530/x3rgljnc5tmn0uzu7znz.png"
+        imgsrc: "https://res.cloudinary.com/dlmwcu9am/image/upload/v1731252530/x3rgljnc5tmn0uzu7znz.png",
+        price: 65.00
     },
     
 ];
@@ -84,9 +99,14 @@ function displayProducts() {
         const productName = document.createElement("h3");
         productName.innerText = product.productName;
 
+        // Product price
+        const productPrice = document.createElement("p");
+        productPrice.innerText = `Price: ${product.price.toFixed(2)} kr.`;
+
         // Add image and product name to the product card
         productItem.appendChild(img);
         productItem.appendChild(productName);
+        productItem.appendChild(productPrice);
 
         // Add the product card to the container
         productContainer.appendChild(productItem);
