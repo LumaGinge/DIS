ddc3e6428b8fc87402641805c1cbb683a535fc4d
Christian <you@example.com>
2024-12-03 18:05:20 +0100

    twilio

--- Routes/twilioRoutes.js
+++ Routes/twilioRoutes.js
@@ -14,16 +14,15 @@ const client = twilio(accountSid, authToken);
 // Route til at sende OTP
 router.post("/send-otp", async (req, res) => {
     const { phoneNumber } = req.body;
-
     try {
         const verification = await client.verify.v2.services(verifyServiceSid)
             .verifications
             .create({ to: phoneNumber, channel: 'sms' });
-        console.log(`OTP sent to ${phoneNumber}`);
-        res.status(200).send("OTP sent successfully");
+        console.log(`One time password sent to ${phoneNumber}`);
+        res.status(200).json("One time password sent successfully");
     } catch (error) {
         console.error(`Failed to send OTP: ${error}`);
-        res.status(500).send("Failed to send OTP");
+        res.status(500).json("Failed to send OTP");
     }
 });
 
@@ -36,13 +35,13 @@ router.post("/verify-otp", async (req, res) => {
             .verificationChecks
             .create({ to: phoneNumber, code: otp });
         if (verification_check.status === "approved") {
-            res.status(200).send("OTP verified successfully");
+            res.status(200).json("Two-factor authentication successful");
         } else {
-            res.status(400).send("Invalid OTP");
+            res.status(400).json("Invalid two-factor authentication code");
         }
     } catch (error) {
-        console.error(`Failed to verify OTP: ${error}`);
-        res.status(500).send("Failed to verify OTP");
+        console.error(`Failed attempt to varify One time password: ${error}`);
+        res.status(500).json("Failed to verify one time password");
     }
 });
 module.exports = router;
