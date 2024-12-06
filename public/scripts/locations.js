2659220e431c34fbe61ab47881c8fb7b0b2bbc7b
LumaGinge <luma23ac@cbs.student.dk>
2024-12-06 16:15:06 +0100

    locations fix

--- public/scripts/locations.js
+++ public/scripts/locations.js
@@ -1,6 +1,6 @@
 async function loadMap() {
     try {
-      const mapUrl = "https://api.mapbox.com/styles/v1/cbseksamen/cm3xaropx00gj01sd843w8j9a.html?title=false&access_token=pk.eyJ1IjoiY2JzZWtzYW1lbiIsImEiOiJjbTN4OWZsNnYxN2Y2MmxxdGhpeGszdWQwIn0.jN9U6R_8wEWKKIgOjCtAbg&zoomwheel=false#10.72/55.6576/12.5702";
+      const mapUrl = "https://api.mapbox.com/styles/v1/cbseksamen/cm3xaropx00gj01sd843w8j9a.html?title=copy&access_token=pk.eyJ1IjoiY2JzZWtzYW1lbiIsImEiOiJjbTN4OWZsNnYxN2Y2MmxxdGhpeGszdWQwIn0.jN9U6R_8wEWKKIgOjCtAbg&zoomwheel=true&fresh=true#10.37/55.6546/12.5665";
       const mapContainer = document.getElementById("map-container");
       const iframe = document.createElement("iframe");
       iframe.width = "100%";
